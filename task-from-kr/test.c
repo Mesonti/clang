5df344016e3d273542d398a2b8de44284407685a
Ivan Samyshev <samyshev.2000@mail.ru>
2023-03-31 14:18:15 +0400

    update 1.20 && 1.21

--- task-from-kr/test.c
+++ task-from-kr/test.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define COUNT 5
+// задача в том, чтобы заполнить массив одним пробелом
 int main()
 {
     int c;
-    int arr[5] = {' ', ' ', ' ', ' ', ' '};
+    int arra[COUNT] = {' ', ' ', ' ', ' ', ' '};
 
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < COUNT; ++i)
     {
-        if (arr[i] == ' ')
+        if (arra[i] == ' ')
         {
-            arr[i] = 0;
-            printf("array[%d] = %d\n", i, arr[i]);
+            arra[i] = '\0';
+            printf("array[%d] = %c\n", i, arra[i]);
         }
-        arr[i] = ' ';
-        printf("array update[%d] = %d\n", i, arr[i]);
         // array[1] = {' '};
         // array[2] = {' '};
         // array[3] = {' '};
         // array[4] = {' '};
         // array[5] = {' '};
+        // есть вариант почистить массив от всех элементов, а затем заполнить его пробелом
     }
+    // я понял как обновить массив -- просто обнули его, а потом положи новый элемент
+    arra[COUNT] = ' ';
+    printf("array update[%d] = 2%c2222\n", COUNT, arra[COUNT]);
 }
