38be001da04fa9abdf2d9a9649add8f213e744d6
Ivan Samyshev <samyshev.2000@mail.ru>
2023-04-09 11:56:41 +0400

    complete 2.1.c

--- task-from-kr/2.1.c
+++ task-from-kr/2.1.c
@@ -7,25 +7,30 @@ int main()
     // char, short, int, long
     // signed | unsigned
     printf("Long ");
-    printf("%d %d\n", LONG_MIN, LONG_MAX);
-    printf("0 %u\n", ULLONG_MAX);
+    printf("%d %d\n", LONG_MIN, LONG_MAX); // -2147483648 2147483647
+    printf("Unsigned 0 %u\n", ULLONG_MAX);
 
     printf("Int ");
-    printf("%d %d\n", INT_MIN, INT_MAX);
-    printf("0 %u\n", UINT_MAX);
-
+    printf("%d %d\n", INT_MIN, INT_MAX); // -2147483648 2147483647
+    printf("Unsigned 0 %u\n", UINT_MAX);
 
     printf("Char ");
     printf("%d %d\n", CHAR_MIN, CHAR_MAX); // -128 127
-    printf("0 %u\n", UCHAR_MAX);
+    printf("Unsigned 0 %u\n", UCHAR_MAX);
 
     printf("Short ");
-    printf("%d %d\n", SHRT_MIN, SHRT_MAX);
-    printf("0 %u\n", USHRT_MAX);
+    printf("%d %d\n", SHRT_MIN, SHRT_MAX); // -32768 32767
+    printf("Unsigned 0 %u\n", USHRT_MAX);
+    printf("%d\n", sizeof(short));
 
-    printf("Double ");
-    printf("%d", );
+    printf("Float: ");
+    printf("%d\n", FLT_MAX);
+    printf("Unsigned: %u\n", FLT_MAX);
 
+    printf("Double: ");
+    printf("%d\n", DBL_MAX_10_EXP);
+    printf("Unsigned: %u", DBL_MAX);
 
-
+    // 1 byte = 8 bit
+    // 8 bit = 255 value;
 }
